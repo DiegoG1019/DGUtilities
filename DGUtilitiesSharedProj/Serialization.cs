38d6e1b46978beecc9da9f91e6a1e7f322ad5929
DiegoG1016 <dagarciam1014@gmail.com>
2020-09-26 20:55:13 -0400

    -Made the library C# 7 Compliant -Added Async methods

--- DGUtilitiesSharedProj/Serialization.cs
+++ DGUtilitiesSharedProj/Serialization.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Text;
 using System.Text.Json;
+using System.Threading.Tasks;
 using System.Xml.Serialization;
 
 namespace DiegoG.Utilities
@@ -11,50 +12,89 @@ namespace DiegoG.Utilities
     {
         public const string XmlExtension = ".xml";
         public const string JsonExtension = ".json";
-        public static class Deserialize
+        public static class Deserialize<T>
         {
-            public static T Xml<T>(string xmlString)
+            public static T Xml(string xmlString)
             {
                 var t = typeof(T);
                 var serializer = new XmlSerializer(t);
-                using StringReader sr = new StringReader(xmlString);
-                return (T)serializer.Deserialize(sr);
+                using(StringReader sr = new StringReader(xmlString))
+                {
+                    return (T)serializer.Deserialize(sr);
+                }
             }
-            public static T Xml<T>(string path, string file)
+            public static T Xml(string path, string file)
             {
                 string fullpath = Path.Combine(path, file + XmlExtension);
                 using (StreamReader InFile = new StreamReader(new FileStream(fullpath, FileMode.Open, FileAccess.Read, FileShare.Read)))
                 {
                     string xmlstring = InFile.ReadToEnd();
-                    return Xml<T>(xmlstring);
+                    return Xml(xmlstring);
                 }
             }
 
-            public static T Json<T>(string jsonString)
+            public static async Task<T> XmlAsync(string xmlString)
+            {
+                var tsk = new Task<T>(new Func<T>(delegate { return Xml(xmlString); }));
+                return await tsk;
+            }
+            public static async Task<T> XmlAsync(string path, string file)
+            {
+                var tsk = new Task<T>(new Func<T>(delegate { return Xml(path, file); }));
+                return await tsk;
+            }
+
+            public static T Json(string jsonString)
             {
                 return JsonSerializer.Deserialize<T>(jsonString);
             }
-            public static T Json<T>(string path, string file)
+            public static T Json(string path, string file)
             {
                 string fullpath = Path.Combine(path, file + JsonExtension);
                 using (StreamReader InFile = new StreamReader(new FileStream(fullpath, FileMode.Open, FileAccess.Read, FileShare.Read)))
                 {
                     string jsonstring = InFile.ReadToEnd();
-                    return Json<T>(jsonstring);
+                    return Json(jsonstring);
                 }
             }
+
+            public static async Task<T> JsonAsync(string jsonString)
+            {
+                var tsk = new Task<T>(new Func<T>(delegate { return Json(jsonString); }));
+                return await tsk;
+            }
+            public static async Task<T> JsonAsync(string path, string file)
+            {
+                var tsk = new Task<T>(new Func<T>(delegate { return Json(path, file); }));
+                return await tsk;
+            }
+
         }
 
         public static class Serialize
         {
+
+            public static async Task<string> XmlAsync(object obj)
+            {
+                var tsk = new Task<string>(new Func<string>(delegate { return Xml(obj); }));
+                return await tsk;
+            }
+            public static async Task<string> XmlAsync(object obj, string path, string file)
+            {
+                var tsk = new Task<string>(new Func<string>(delegate { return Xml(obj, path, file); }));
+                return await tsk;
+            }
+
             public static string Xml(object obj)
             {
                 var t = obj.GetType();
                 var serializer = new XmlSerializer(t);
-                using MemoryStream ms = new MemoryStream();
-                serializer.Serialize(ms, obj);
-                ms.Position = 0;
-                return new StreamReader(ms).ReadToEnd();
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    serializer.Serialize(ms, obj);
+                    ms.Position = 0;
+                    return new StreamReader(ms).ReadToEnd();
+                }
             }
             public static string Xml(object obj, string path, string file)
             {
@@ -65,6 +105,17 @@ namespace DiegoG.Utilities
                     OutFile.WriteLine(xmlstring);
                 }
                 return xmlstring;
+            }
+
+            public static async Task<string> JsonAsync(object obj)
+            {
+                var tsk = new Task<string>(new Func<string>(delegate { return Json(obj); }));
+                return await tsk;
+            }
+            public static async Task<string> JsonAsync(object obj, string path, string file)
+            {
+                var tsk = new Task<string>(new Func<string>(delegate { return Json(obj, path, file); }));
+                return await tsk;
             }
 
             public static string Json(object obj)
