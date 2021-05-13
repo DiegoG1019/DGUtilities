8907bd145aa05ca86dcf8a02f1c53ec886e68cb8
Diego García <dagarciam1014@gmail.com>
2021-05-13 14:34:29 -0400

    Update ExtensionLoader.cs

--- DiegoG.Utilities.Reflection/ExtensionLoader.cs
+++ DiegoG.Utilities.Reflection/ExtensionLoader.cs
@@ -12,7 +12,19 @@ namespace DiegoG.Utilities.Reflection
 {
     public static class ExtensionLoader
     {
+        private static string ExtensionsDir;
+        private static AppDomain ExtensionsDomain;
+        private readonly static List<string> LoadedExtensionsList = new();
+        
+        public static IEnumerable<string> LoadedExtensions => LoadedExtensionsList;
         public static bool IsInit { get; private set; } = false;
+
+        private static void ThrowIfNotInit()
+        {
+            if (!IsInit)
+                throw new InvalidOperationException("Initialize the class first");
+        }
+
         public static void Initialize(string extensionDir)
         {
             if (IsInit)
@@ -22,28 +34,31 @@ namespace DiegoG.Utilities.Reflection
             ExtensionsDir = extensionDir;
             ExtensionsDomain = AppDomain.CurrentDomain;
         }
-        private static string ExtensionsDir;
-        private static AppDomain ExtensionsDomain;
-        private readonly static List<string> LoadedExtensionsList = new();
-        public static IEnumerable<string> LoadedExtensions => LoadedExtensionsList;
-        public static void Load()
+
+        public static IEnumerable<string> EnumerateUnloadedAssemblies()
         {
-            if (!IsInit)
-                throw new InvalidOperationException("Initialize the class first");
+            ThrowIfNotInit();
+            return EnumerateAssemblies().Where(s => !LoadedExtensionsList.Contains(s));
+        }
+
+        public static IEnumerable<string> EnumerateAssemblies()
+        {
+            ThrowIfNotInit();
+            Directory.CreateDirectory(ExtensionsDir);
+            return Directory.EnumerateFiles(ExtensionsDir, "*.dll", SearchOption.AllDirectories);
+        }
+
+        public static void Load(IEnumerable<string> paths)
+        {
             var count = 0;
             var ch = new Stopwatch();
             ch.Start();
-            Log.Information("Reloading extensions");
-            foreach 
-            (
-                var file in 
-                Directory.EnumerateFiles(ExtensionsDir, "*.dll", SearchOption.AllDirectories)
-                .Where(s=>!LoadedExtensionsList.Contains(s))
-            )
+            Log.Information("Loading Extensions");
+            foreach(var file in paths)
             {
                 Log.Debug("Loading Assembly: " + file);
                 var asm = Assembly.LoadFrom(file);
-                Log.Debug("Loading assembly into " + nameof(ExtensionsDomain));
+                Log.Debug("Loading assembly into AppDomain");
                 ExtensionsDomain.Load(asm.GetName(false));
                 LoadedExtensionsList.Add(file);
                 count++;
