e21c2334abea353f6e3d3522384ebd77861cfecf
Diego García <dagarciam1014@gmail.com>
2021-09-17 00:25:34 -0400

    Create GenericEventArgs.cs

---
+++ DiegoG.MonoGame/GenericEventArgs.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiegoG.MonoGame
+{
+    public class GenericEventArgs<T> : EventArgs
+    {
+        public T Data { get; init; }
+        public GenericEventArgs(T data) => Data = data;
+    }
+}
